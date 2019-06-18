5b2a811db904d5b1164719911413af3763338516
chetchopra <chetanya.chopra94@gmail.com>
2019-06-18 13:35:07 -0700

    Added image_url column to items table

--- shop-project/db/schema.rb
+++ shop-project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_003606) do
+ActiveRecord::Schema.define(version: 2019_06_18_203140) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_003606) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "shopinventories", force: :cascade do |t|
