1d6391c4ecde9b97276f6032d145a09ac0b5ddc1
chetchopra <chetanya.chopra94@gmail.com>
2019-06-18 16:20:24 -0700

    Added in login functionality

--- shop-project/config/routes.rb
+++ shop-project/config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  resources :travelers
+  resources :travelers, except: [:new, :create]
   resources :travelerinventories
   resources :transactions
   resources :shopkeepers
   resources :shopinventories
   resources :items
   resources :categories
+
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'travelers#new'
+  post '/signup', to: 'travelers#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
